e9e8969e0565397f637e4259ced313dafe824e6e
Sabitov Renat <Renat.Sabitov@russianpost.ru>
2025-09-27 23:22:20 +0300

    Authorize, Views/Admin

---
+++ Coffee/Controllers/Admin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class Admin : Controller
+    {
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
